688952f295633be21c7b93ec1cb07857ad13b9b3
Devin Mounts and Ryan Putman <ryan@>
2018-07-31 11:09:58 -0700

    add board contructor

---
+++ src/sudoku.js
@@ -0,0 +1,25 @@
+export function Board (){
+var rowA = [5, 3, , , 7, , , , ];
+var rowB = [6, , , 1, 9, 5, , , ];
+var rowC = [ , 9, 8, , , , , 6, ];
+var rowD = [8, , , , 6, , , , 3];
+var rowE = [4, , , 8, , 3, , , 1];
+var rowF = [ , , , , 2, , , , 6];
+var rowG = [ , 6, , , , , 2, 8, ];
+var rowH = [ , , , 4, 1, 9, , , 5];
+var rowI = [ , , , , 8, , , 7, 9];
+
+var col1 = [5, 6, , 8, 4, , , , ];
+var col2 = [3, , 9, , , , 6, , ];
+var col3 = [ , , 8, , , , , , ];
+var col4 = [ , 1, , , 8, , , 4, ];
+var col5 = [7, 9, , 6, , 2, , 1, 8];
+var col6 = [ , 5, , , 3, , , 9, ];
+var col7 = [ , , , , , , 2, , ];
+var col8 = [ , , 6, , , , 8, , 7];
+var col9 = [ , , , 3, 1, 6, , 5, 9];  
+}
+
+export function checkRow() {
+
+}
