07ee9263d11fc2691655f67b4d1d10a59ac49a1b
Devin Mounts and Ryan Putman <devin@>
2018-07-31 16:48:12 -0700

    add visual frontend

--- src/main.js
+++ src/main.js
@@ -9,31 +9,31 @@ $(document).ready(function() {
   $('#sudoku').submit(function(event){
     event.preventDefault();
 
-    var rowA = [$('#cell-0').val(), $('#cell-1').val(), $('#cell-2').val(), $('#cell-3').val(), $('#cell-4').val(), $('#cell-5').val(), $('#cell-6').val(), $('#cell-7').val(), $('#cell-8').val()];
+    var rowA = [parseInt($('#cell-0').val()), parseInt($('#cell-1').val()), parseInt($('#cell-2').val()), parseInt($('#cell-3').val()), parseInt($('#cell-4').val()), parseInt($('#cell-5').val()), parseInt($('#cell-6').val()), parseInt($('#cell-7').val()), parseInt($('#cell-8').val())];
 
 
-    var rowB= [$('#cell-9').val(), $('#cell-10').val(), $('#cell-11').val(), $('#cell-12').val(), $('#cell-13').val(), $('#cell-14').val(), $('#cell-15').val(), $('#cell-16').val(), $('#cell-17').val()];
+    var rowB= [parseInt($('#cell-9').val()), parseInt($('#cell-10').val()), parseInt($('#cell-11').val()), parseInt($('#cell-12').val()), parseInt($('#cell-13').val()), parseInt($('#cell-14').val()), parseInt($('#cell-15').val()), parseInt($('#cell-16').val()), parseInt($('#cell-17').val())];
 
 
-    var rowC= [$('#cell-18').val(), $('#cell-19').val(), $('#cell-20').val(), $('#cell-21').val(), $('#cell-22').val(), $('#cell-23').val(), $('#cell-24').val(), $('#cell-25').val(), $('#cell-26').val()];
+    var rowC= [parseInt($('#cell-18').val()), parseInt($('#cell-19').val()), parseInt($('#cell-20').val()), parseInt($('#cell-21').val()), parseInt($('#cell-22').val()), parseInt($('#cell-23').val()), parseInt($('#cell-24').val()), parseInt($('#cell-25').val()), parseInt($('#cell-26').val())];
 
 
-    var rowD= [$('#cell-27').val(), $('#cell-28').val(), $('#cell-29').val(), $('#cell-30').val(), $('#cell-31').val(), $('#cell-32').val(), $('#cell-33').val(), $('#cell-34').val(), $('#cell-35').val()];
+    var rowD= [parseInt($('#cell-27').val()), parseInt($('#cell-28').val()), parseInt($('#cell-29').val()), parseInt($('#cell-30').val()), parseInt($('#cell-31').val()), parseInt($('#cell-32').val()), parseInt($('#cell-33').val()), parseInt($('#cell-34').val()), parseInt($('#cell-35').val())];
 
 
-    var rowE= [$('#cell-36').val(), $('#cell-37').val(), $('#cell-38').val(), $('#cell-39').val(), $('#cell-40').val(), $('#cell-41').val(), $('#cell-42').val(), $('#cell-43').val(), $('#cell-44').val()];
+    var rowE= [parseInt($('#cell-36').val()), parseInt($('#cell-37').val()), parseInt($('#cell-38').val()), parseInt($('#cell-39').val()), parseInt($('#cell-40').val()), parseInt($('#cell-41').val()), parseInt($('#cell-42').val()), parseInt($('#cell-43').val()), parseInt($('#cell-44').val())];
 
 
-    var rowF= [$('#cell-45').val(), $('#cell-46').val(), $('#cell-47').val(), $('#cell-48').val(), $('#cell-49').val(), $('#cell-50').val(), $('#cell-51').val(), $('#cell-52').val(), $('#cell-53').val()];
+    var rowF= [parseInt($('#cell-45').val()), parseInt($('#cell-46').val()), parseInt($('#cell-47').val()), parseInt($('#cell-48').val()), parseInt($('#cell-49').val()), parseInt($('#cell-50').val()), parseInt($('#cell-51').val()), parseInt($('#cell-52').val()), parseInt($('#cell-53').val())];
 
 
-    var rowG= [$('#cell-54').val(), $('#cell-55').val(), $('#cell-56').val(), $('#cell-57').val(), $('#cell-58').val(), $('#cell-59').val(), $('#cell-60').val(), $('#cell-61').val(), $('#cell-62').val()];
+    var rowG= [parseInt($('#cell-54').val()), parseInt($('#cell-55').val()), parseInt($('#cell-56').val()), parseInt($('#cell-57').val()), parseInt($('#cell-58').val()), parseInt($('#cell-59').val()), parseInt($('#cell-60').val()), parseInt($('#cell-61').val()), parseInt($('#cell-62').val())];
 
 
-    var rowH= [$('#cell-63').val(), $('#cell-64').val(), $('#cell-65').val(), $('#cell-66').val(), $('#cell-67').val(), $('#cell-68').val(), $('#cell-69').val(), $('#cell-70').val(), $('#cell-71').val()];
+    var rowH= [parseInt($('#cell-63').val()), parseInt($('#cell-64').val()), parseInt($('#cell-65').val()), parseInt($('#cell-66').val()), parseInt($('#cell-67').val()), parseInt($('#cell-68').val()), parseInt($('#cell-69').val()), parseInt($('#cell-70').val()), parseInt($('#cell-71').val())];
 
 
-    var rowI= [$('#cell-72').val(), $('#cell-73').val(), $('#cell-74').val(), $('#cell-76').val(), $('#cell-77').val(), $('#cell-78').val(), $('#cell-79').val(), $('#cell-80').val(), $('#cell-71').val()];
+    var rowI= [parseInt($('#cell-72').val()), parseInt($('#cell-73').val()), parseInt($('#cell-74').val()), parseInt($('#cell-76').val()), parseInt($('#cell-77').val()), parseInt($('#cell-78').val()), parseInt($('#cell-79').val()), parseInt($('#cell-80').val()), parseInt($('#cell-71').val())];
 
 
     var board = Board(rowA, rowB, rowC, rowD, rowE, rowF, rowG, rowH, rowI);
--- src/sudoku.js
+++ src/sudoku.js
@@ -1,54 +1,58 @@
 export function Board (rowA, rowB, rowC, rowD, rowE, rowF, rowG, rowH, rowI){
   var allArrays = [];
 
-  var col1 = [5, 6, 1, 8, 4, 7, 9, 2, 3];
-  var col2 = [3, 7, 9, 5, 2, 1, 6, 8, 4];
-  var col3 = [ 4, 2, 8, 9, 6, 3, 1, 7, 5];
-  var col4 = [ 6, 1, 3, 7, 8, 9, 5, 4, 2];
-  var col5 = [7, 9, 4, 6, 5, 2, 3, 1, 8];
-  var col6 = [ 8, 5, 2, 1, 3, 4, 7, 9, 6];
-  var col7 = [ 9, 3, 5, 4, 7, 8, 2, 6, 1];
-  var col8 = [ 1, 4, 6, 2, 9, 5, 8, 3, 7];
-  var col9 = [ 2, 8, 7, 3, 1, 6, 4, 5, 9];
+  var col1 = [rowA[0], rowB[0], rowC[0], rowD[0], rowE[0], rowF[0], rowG[0], rowH[0], rowI[0]];
+  var col2 = [rowA[1], rowB[1], rowC[1], rowD[1], rowE[1], rowF[1], rowG[1], rowH[1], rowI[1]];
+  var col3 = [rowA[2], rowB[2], rowC[2], rowD[2], rowE[2], rowF[2], rowG[2], rowH[2], rowI[2]];
+  var col4 = [rowA[3], rowB[3], rowC[3], rowD[3], rowE[3], rowF[3], rowG[3], rowH[3], rowI[3]];
+  var col5 = [rowA[4], rowB[4], rowC[4], rowD[4], rowE[4], rowF[4], rowG[4], rowH[4], rowI[4]];
+  var col6 = [rowA[5], rowB[5], rowC[5], rowD[5], rowE[5], rowF[5], rowG[5], rowH[5], rowI[5]];
+  var col7 = [rowA[6], rowB[6], rowC[6], rowD[6], rowE[6], rowF[6], rowG[6], rowH[6], rowI[6]];
+  var col8 = [rowA[7], rowB[7], rowC[7], rowD[7], rowE[7], rowF[7], rowG[7], rowH[7], rowI[7]];
+  var col9 = [rowA[8], rowB[8], rowC[8], rowD[8], rowE[8], rowF[8], rowG[8], rowH[8], rowI[8]];
 
-  var sq1 = [1,2,3,4,5,6,7,8,9];
-  var sq2 = [1,2,3,4,5,6,7,8,9];
-  var sq3 = [1,2,3,4,5,6,7,8,9];
-  var sq4 = [1,2,3,4,5,6,7,8,9];
-  var sq5 = [1,2,3,4,5,6,7,8,9];
-  var sq6 = [1,2,3,4,5,6,7,8,9];
-  var sq7 = [1,2,3,4,5,6,7,8,9];
-  var sq8 = [1,2,3,4,5,6,7,8,9];
-  var sq9 = [1,2,3,4,5,6,7,8,9];
+  var sq1 = [rowA[0],rowA[1],rowA[2],rowB[0],rowB[1],rowB[2],rowC[0],rowC[1],rowC[2]];
+  var sq2 = [rowA[3],rowA[4],rowA[5],rowB[3],rowB[4],rowB[5],rowC[3],rowC[4],rowC[5]];
+  var sq3 = [rowA[6],rowA[7],rowA[8],rowB[6],rowB[7],rowB[8],rowC[6],rowC[7],rowC[8]];
+  var sq4 = [rowD[0],rowD[1],rowD[2],rowE[0],rowE[1],rowE[2],rowF[0],rowF[1],rowF[2]];
+  var sq5 = [rowD[3],rowD[4],rowD[5],rowE[3],rowE[4],rowE[5],rowF[3],rowF[4],rowF[5]];
+  var sq6 = [rowD[6],rowD[7],rowD[8],rowE[6],rowE[7],rowE[8],rowF[6],rowF[7],rowF[8]];
+  var sq7 = [rowG[0],rowG[1],rowG[2],rowH[0],rowH[1],rowH[2],rowI[0],rowI[1],rowI[2]];
+  var sq8 = [rowG[3],rowG[4],rowG[5],rowH[3],rowH[4],rowH[5],rowI[3],rowI[4],rowI[5]];
+  var sq9 = [rowG[6],rowG[7],rowG[8],rowH[6],rowH[7],rowH[8],rowI[6],rowI[7],rowI[8]];
 
   allArrays.push(rowA, rowB, rowC, rowD, rowE, rowF, rowG, rowH, rowI, col1, col2, col3, col4, col5, col6, col7, col8, col9, sq1, sq2, sq3, sq4, sq5, sq6, sq7, sq8, sq9);
   return allArrays;
 }
 
 export function checkBoard(allArrays) {
-var allResults = [];
-  allArrays.forEach(array => {
+  var allResults = [];
+
+  allArrays.forEach(function(array){
+
     var result = checkRow(array);
     allResults.push(result);
   });
-if(allResults.includes(false)){
-  return false;
-}
+
+  if(allResults.includes(false)){
+    return false;
+  }
   return true;
 }
 
 export function checkRow(array) {
-  // allArrays.forEach(function(array){
-    for(var x = 1; x <= 9; x++){
-      var index = array.indexOf(x);
-      if(index != -1){
+
+  for(var x = 1; x <= 9; x++){
+    debugger;
+    var index = array.indexOf(x);
+    if(index != -1){
       array.splice(index, 1);
-      }
     }
-    if (array.length != 0) {
-      return false;
-    }
-    else {
-      return true;
-    }
+  }
+  if (array.length != 0) {
+    return false;
+  }
+  else {
+    return true;
+  }
 }
